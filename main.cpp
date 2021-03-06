79cfa47ce774293be3b83f9955fd43b96594499c
flash244 <jakob20022404@gmail.com>
2021-03-06 12:53:21 +0200

    added struct

--- main.cpp
+++ main.cpp
@@ -5,23 +5,53 @@
 #include <fstream>
 using namespace std;
 using namespace filesystem;
+void do_the_stuff(ifstream &current_file);
+int number(ifstream &current_file);
 
+struct Country{
+  string name;
+  int marks[20];
+  int points;
+}
+
+vector <Country> countries;
 
 int main()
 {
-  ifstream currFile;
+  ifstream current_file;
+  ofstream result_file;
   string path;
-  getline(cin, path);
-  vector <string> files;
+  cin >> path;
+  result_file.open(path + "result.csv");
+  vector <string> file_names;
   for (const auto & entry : directory_iterator(path))
     if (entry.path().extension() == ".csv"){
       //cout << entry.path() << endl;
-      files.push_back(entry.path());
+      file_names.push_back(entry.path());
     }
-  for (size_t i = 0; i < files.size(); i++) {
-    currFile.open(files[i]);
-    if (!currFile.is_open()) {
+  for (size_t i = 0; i < file_names.size(); i++) {
+    current_file.open(file_names[i]);
+    if (!current_file.is_open()) {
       cout << "File isn't open!" << endl;
     }
+    do_the_stuff(current_file);
+    current_file.close();
   }
+}
+
+void do_the_stuff(ifstream &current_file){
+  int numb = number(current_file);
+  string current_line;
+  for (size_t i = 0; i < numb; i++) {
+    getline(current_file, current_line);
+    //cout << str << endl;
+    string country_name = current_line.substr(0, current_line.find(','));
+    cout << country_name << endl;
+  }
+}
+
+int number(ifstream &current_file){
+  string str;
+  getline(current_file, str);
+  return stoi(str);
 }
