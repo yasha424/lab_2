a7c0078eab1b6191bfb7ec649bbb5e4f6b9243bc
flash244 <jakob20022404@gmail.com>
2021-03-06 15:29:55 +0200

    almost done

--- main.cpp
+++ main.cpp
@@ -18,6 +18,9 @@ int number(ifstream &current_file);
 string word_before_comma(string &word);
 Country work_with_line(string current_line);
 void sort();
+void cout_all_struct();
+void sort_last_time();
+
 
 vector <Country> countries;
 
@@ -42,6 +45,9 @@ int main()
     do_the_stuff(current_file);
     current_file.close();
   }
+  sort();
+  sort_last_time();
+  cout_all_struct();
 }
 
 void do_the_stuff(ifstream &current_file){
@@ -76,18 +82,38 @@ void sort(){
   for (size_t i = 0; i < countries.size(); i++) {
     for (size_t j = 0; j < countries.size() - 1; j++) {
       for (size_t k = 0; k < countries.size() - 1; k++) {
-        if (countries[i].marks[k] < countries[i].marks[k+1]) {
-          swap(countries[i].marks[k], countries[i].marks[k+1]);
+        if (countries[k].marks[i] < countries[k+1].marks[i]) {
+          swap(countries[k], countries[k+1]);
         }
       }
+
     }
+    // cout << i << endl;
+    // cout_all_struct();
     for (size_t l = 0; l < 10; l++) {
-      countries[i].points += points[l];
+      countries[l].points += points[l];
     }
   }
 }
+
+void sort_last_time(){
+  for (size_t i = 0; i < countries.size(); i++) {
+    for (size_t j = 0; j < countries.size() - 1; j++) {
+      if (countries[j].points < countries[j+1].points) {
+        swap(countries[j], countries[j+1]);
+      }
+    }
+  }
+}
+
 int number(ifstream &current_file){
   string str;
   getline(current_file, str);
   return stoi(str);
+}
+
+void cout_all_struct(){
+  for (size_t i = 0; i < countries.size(); i++) {
+    cout << countries[i].name << "  " << countries[i].points << endl;
+  }
 }
