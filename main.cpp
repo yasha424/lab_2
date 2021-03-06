1019832caa81f5d314223a136b10a6a4b05de062
flash244 <jakob20022404@gmail.com>
2021-03-06 16:40:01 +0200

    added descriptions

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <fstream>
 using namespace std;
-using namespace filesystem/* = std::filesystem*/;
+using namespace filesystem;
 
 struct Country{
   string name;
@@ -16,8 +16,7 @@ void do_the_stuff(ifstream &current_file);
 int number(ifstream &current_file);
 string word_before_comma(string &word);
 Country work_with_line(string current_line);
-void sort();
-//void cout_all_struct();
+void points_adder();
 void sort_last_time();
 void write_in_file(ofstream &result_file);
 
@@ -34,7 +33,6 @@ int main()
   vector <string> file_names;
   for (const auto & entry : directory_iterator(path))
     if (entry.path().extension() == ".csv"){
-      //cout << entry.path() << endl;
       file_names.push_back(entry.path());
     }
   for (size_t i = 0; i < file_names.size(); i++) {
@@ -45,29 +43,30 @@ int main()
     do_the_stuff(current_file);
     current_file.close();
   }
-  sort();
+  points_adder();
   sort_last_time();
-  //cout_all_struct();
   write_in_file(result_file);
 }
 
+// функция, которая делает всю работу xD
 void do_the_stuff(ifstream &current_file){
   int numb = number(current_file);
   string current_line;
   for (size_t i = 0; i < numb; i++) {
     getline(current_file, current_line);
-    //cout << str << endl;
-    // string country_name = current_line.substr(0, current_line.find(','));
-    // cout << country_name << endl;
     countries.push_back(work_with_line(current_line));
   }
 }
+
+//функция, которая возвращает слово, разделенное запятой
 string word_before_comma(string &word) {
     int pos = word.find(",");
     string subword = word.substr(0, pos);
     word = word.substr(pos+1);
     return subword;
 }
+
+// функция6 возвращающая инфу про страну
 Country work_with_line(string current_line) {
     Country currCountry;
     currCountry.name = word_before_comma(current_line);
@@ -78,7 +77,9 @@ Country work_with_line(string current_line) {
     }
     return currCountry;
 }
-void sort(){
+
+// функция, добавляющая очки странам
+void points_adder(){
   int points[] = {12, 10, 8, 7, 6, 5, 4, 3, 2, 1};
   for (size_t i = 0; i < countries.size(); i++) {
     for (size_t j = 0; j < countries.size() - 1; j++) {
@@ -89,14 +90,13 @@ void sort(){
       }
 
     }
-    // cout << i << endl;
-    // cout_all_struct();
     for (size_t l = 0; l < 10; l++) {
       countries[l].points += points[l];
     }
   }
 }
 
+// функция сортирующая страны по набраным очкам
 void sort_last_time(){
   for (size_t i = 0; i < countries.size(); i++) {
     for (size_t j = 0; j < countries.size() - 1; j++) {
@@ -107,18 +107,14 @@ void sort_last_time(){
   }
 }
 
+// функция, которая возвращает кол-во стран
 int number(ifstream &current_file){
   string str;
   getline(current_file, str);
   return stoi(str);
 }
 
-// void cout_all_struct(){
-//   for (size_t i = 0; i < countries.size(); i++) {
-//     cout << countries[i].name << "  " << countries[i].points << endl;
-//   }
-// }
-
+//функция записи в файл
 void write_in_file(ofstream &result_file){
   for (size_t i = 0; i < 10; i++) {
     result_file << countries[i].name << "," << countries[i].points << endl;
